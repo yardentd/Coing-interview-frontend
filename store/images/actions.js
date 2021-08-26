acc26184b917194cd19a832f86120d9a82b6de18
Yarden <70089477+yardentd@users.noreply.github.com>
2021-08-26 12:27:12 +0300

    upload frontend  project

--- store/images/actions.js
+++ store/images/actions.js
@@ -1,3 +1,8 @@
+
+const URL = 'http://localhost:9090/v1/images';
 export default {
-  async getImagesFromApi ({ commit }) {}
+
+  async getImagesFromApi ({ commit }) {
+    await fetch(URL).then(res => res.json()).then(data => commit('setImages', data));
+  }
 };
